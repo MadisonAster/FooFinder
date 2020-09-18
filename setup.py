3cffe654eaa526a2fdbef6ec53e7564f0a1807ae
Madison Aster <info@MadisonAster.com>
2020-09-17 22:17:36 -0700

    removing 3.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='A package designed to help you find foo.',
     long_description=readme(),
     long_description_content_type='text/markdown',
-    python_requires='>3.4.0',
+    python_requires='>3.5.0',
     url='https://github.com/MadisonAster/FooFinder',
     author='Madison Aster',
     license='GPL',
@@ -22,7 +22,12 @@ setup(
             'Intended Audience :: Developers',
             'Topic :: Software Development',
             'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            
             ],
 
     keywords='staticframe pandas numpy immutable array',
