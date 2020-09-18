93c51224b278470049e8714480606f00ad52fd30
Madison Aster <info@MadisonAster.com>
2020-09-18 01:22:48 -0700

    adding fixes

--- FooFinder/FooFinder/__init__.py
+++ FooFinder/FooFinder/__init__.py
@@ -1,22 +1,36 @@
-import sys, os, builtins, inspect
+import sys, os, builtins, inspect, traceback
 from importlib import machinery
-from pprint import pprint
+
+def _loadmodule(name, path):
+    module = machinery.SourceFileLoader(name, path).load_module()
+    sys.modules[name] = module
+    globals()[name] = module
+    return module
 
 def _import(name, *args, **kwargs):
     if name != 'FooFinder' or not args[2]:
         return original_import(name, *args, **kwargs)
     name = args[2][0]
+    if name in globals():
+        return sys.modules['FooFinder']
+    elif name in sys.modules:
+        globals()[name] = sys.modules[name]
+        return sys.modules['FooFinder']
     frame = inspect.currentframe().f_back
     cwd = os.path.dirname(os.path.abspath(frame.f_globals['__file__'])).replace('\\','/').rstrip('/')
     for i in range(len(cwd.rsplit('/'))):
         folder = cwd.rsplit('/',i)[0]
         path = folder+'/'+name+'.py'
         if os.path.exists(path):
-            module = machinery.SourceFileLoader(name, path).load_module()
-            sys.modules[name] = module
-            globals()[name] = module
+            module = _loadmodule(name, path)
             break
-    return sys.modules[__name__]
+    else:
+        for root, dirs, files in os.walk(cwd):
+            path = root+'/'+name+'.py'
+            if os.path.exists(path):
+                module = _loadmodule(name, path)
+                break
+    return sys.modules['FooFinder']
 
 original_import = builtins.__import__
 builtins.__import__ = _import
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
     return filetext
 setup(
     name='FooFinder',
-    version='0.9.1.10',
+    version='0.9.9.0',
     description='A package designed to help you find foo.',
     long_description=readme(),
     long_description_content_type='text/markdown',
