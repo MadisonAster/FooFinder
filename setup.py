f2b1e17504270252c9903f4671ef547f3a62d06f
Madison Aster <info@MadisonAster.com>
2020-09-17 18:17:31 -0700

    setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     description='A package designed to help you find foo.',
     long_description='FooFinder will walk up to each parent directory from your current module, and look for a module to import with the name you give it.',
-    python_requires='>3.6.0',
+    python_requires='>3.4.0',
     url='https://github.com/MadisonAster/FooFinder',
     author='Madison Aster',
     license='GPL',
