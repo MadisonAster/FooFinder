cffe038c12b03f9ced38259898e4c917f282f7b7
Madison Aster <info@MadisonAster.com>
2020-09-18 09:55:59 -0700

    improving test

--- FooFinder/test.py
+++ FooFinder/test.py
@@ -4,6 +4,7 @@ class test_FooFinder(unittest.TestCase):
     def test_1(self):
         from FooFinder import ExampleBaseModule
         import inspect #prove we're only affecting FooFinder imports
+        import FooFinder #make sure this doesn't throw errors
         i = ExampleBaseModule.ExampleBaseClass()
         self.assertEqual(i.ExampleAttribute, 'Hello World!')
 
