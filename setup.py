a02e8b6143ef1adcd856528e48f99dc00f48a2e4
Madison Aster <info@MadisonAster.com>
2020-09-23 11:40:49 -0700

    trying a refactor

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
     return filetext
 setup(
     name='FooFinder',
-    version='2.2.0',
+    version='2.2.1',
     description='A package designed to help you find foo.',
     long_description=readme(),
     long_description_content_type='text/markdown',
