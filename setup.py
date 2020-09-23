386f427a2b210a9fb3330e9e9d15c96cbdda84ac
Madison Aster <info@MadisonAster.com>
2020-09-23 10:08:20 -0700

    lets give it a shot

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
     return filetext
 setup(
     name='FooFinder',
-    version='2.1.0',
+    version='2.1.1',
     description='A package designed to help you find foo.',
     long_description=readme(),
     long_description_content_type='text/markdown',
