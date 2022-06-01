aa166c016406ef3c01ba8d85996c6a2415a81d60
Madison Aster <info@MadisonAster.com>
2022-05-31 23:44:39 -0700

    3.0.45

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
     return filetext
 setup(
     name='FooFinder',
-    version='3.0.44',
+    version='3.0.45',
     description='A package designed to help you find foo.',
     long_description=readme(),
     long_description_content_type='text/markdown',
@@ -29,6 +29,7 @@ setup(
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
             ],
 
     keywords='relative imports importing recursive',
